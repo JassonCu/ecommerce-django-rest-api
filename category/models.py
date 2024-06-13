ba79f2d7fa7ff942d663a10360aba6c595ec1fb4
Jasson Rolando Cu <jassoncucaal14@hotmail.com>
2024-06-13 08:22:28 -0600

    feat: :art: firts commit

---
+++ category/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+# Create your models here.
+
+
+class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    parent = models.ForeignKey(
+         'self', related_name='children', on_delete=models.CASCADE, blank=True, null=True)
+    name = models.CharField(_('Nombre de categoría'), max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
