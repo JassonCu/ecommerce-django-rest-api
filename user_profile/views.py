2cdffebe786eb56a4e1a26fa094d3e61680c032b
Jasson Rolando Cu <jassoncucaal14@hotmail.com>
2024-07-01 22:24:00 -0600

    feat: :page_facing_up: Added docstrings to `GetUserProfileView`,  `UpdateUserProfileView`

--- user_profile/views.py
+++ user_profile/views.py
@@ -7,7 +7,27 @@ from .serializers import UserProfileSerializer
 # Create your views here.
 
 class GetUserProfileView(APIView):
+    """
+    Vista API para obtener el perfil de usuario.
+
+    Permite a un usuario autenticado recuperar su propio perfil de usuario.
+    """
+
     def get(self, request, format=None):
+        """
+        Maneja peticiones GET para obtener el perfil de usuario.
+
+        Args:
+        - request: Objeto de solicitud enviado por el cliente.
+        - format: Sufijo de formato opcional.
+
+        Returns:
+        - Respuesta con el perfil de usuario o mensaje de error.
+
+        Si el perfil de usuario se encuentra, devuelve una respuesta JSON con el perfil del usuario.
+        Si el perfil de usuario no existe o no se puede recuperar, devuelve un mensaje de error correspondiente.
+        """
+
         try:
             user = self.request.user
             user_profile = UserProfile.objects.get(user=user)
@@ -25,7 +45,27 @@ class GetUserProfileView(APIView):
 
 
 class UpdateUserProfileView(APIView):
+    """
+    Vista API para actualizar el perfil de usuario.
+
+    Permite a un usuario autenticado actualizar su propio perfil de usuario.
+    """
+
     def put(self, request, format=None):
+        """
+        Maneja peticiones PUT para actualizar el perfil de usuario.
+
+        Args:
+        - request: Objeto de solicitud enviado por el cliente.
+        - format: Sufijo de formato opcional.
+
+        Returns:
+        - Respuesta con el perfil de usuario actualizado o mensaje de error.
+
+        Si el perfil de usuario se actualiza correctamente, devuelve una respuesta JSON con el perfil actualizado.
+        Si hay campos faltantes en la solicitud PUT o se produce un error durante la actualización, devuelve un mensaje de error correspondiente.
+        """
+        
         try:
             user = self.request.user
             data = self.request.data
