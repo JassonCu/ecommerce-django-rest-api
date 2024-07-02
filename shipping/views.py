e188619efc980d501a9f2ff3a09da58e8ca87f25
Jasson Rolando Cu <jassoncucaal14@hotmail.com>
2024-07-01 22:25:05 -0600

    feat: :page_facing_up: Added docstrings to `GetShippingView`

--- shipping/views.py
+++ shipping/views.py
@@ -6,10 +6,33 @@ from .serializers import ShippingSerializer
 
 # Create your views here.
 
+
 class GetShippingView(APIView):
+    """
+    Vista API para obtener opciones de envío.
+
+    Este endpoint permite la obtención de opciones de envío ordenadas por precio.
+    """
+
     permission_classes = (permissions.AllowAny, )
 
     def get(self, request, format=None):
+        """
+        Maneja peticiones GET para obtener opciones de envío.
+
+        Args:
+        - request: Objeto de solicitud enviado por el cliente.
+        - format: Sufijo de formato opcional.
+
+        Returns:
+        - Respuesta con opciones de envío o mensaje de error.
+
+        Si existen opciones de envío, devuelve una respuesta JSON con una lista de opciones
+        de envío ordenadas por precio.
+        Si no existen opciones de envío, devuelve una respuesta JSON con un mensaje de error
+        indicando que no hay opciones de envío disponibles.
+        """
+
         if Shipping.objects.all().exists():
             shipping_options = Shipping.objects.order_by('price').all()
             shipping_options = ShippingSerializer(shipping_options, many=True)
