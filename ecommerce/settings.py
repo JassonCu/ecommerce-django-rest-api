e74653c956761dfde055fc7bb94324edd76eb70e
Jasson Rolando Cu <jassoncucaal14@hotmail.com>
2024-07-03 11:32:22 -0600

    feat: :fire: Add `client` app

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -129,17 +129,17 @@ DATABASES = {
 AUTH_USER_MODEL = 'user.UserAccount'
 
 CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000',
-    'http://localhost:8000',
-    'http://127.0.0.1:8000',
-    'http://127.0.0.1:3000',
+    'http://localhost:5173/',
+    'http://localhost:8000/',
+    'http://127.0.0.1:8000/',
+    'http://127.0.0.1:5173/',
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    'http://localhost:3000',
-    'http://localhost:8000',
-    'http://127.0.0.1:8000',
-    'http://127.0.0.1:3000',
+    'http://localhost:5173/',
+    'http://localhost:8000/',
+    'http://127.0.0.1:8000/',
+    'http://127.0.0.1:5173/',
 ]
 
 
